fa12282e62850c52b4887b51e95971f41af54d6b
Omer Zidkoni <omerzi@ad.checkpoint.com>
2022-06-08 13:27:23 +0300

    Checking sites connectivity without using routines

--- main.go
+++ main.go
@@ -1,5 +1,31 @@
 package main
 
+import (
+	"fmt"
+	"net/http"
+)
+
 func main() {
-	
+	sites := []string{
+		"http://google.com",
+		"http://facebook.com",
+		"http://stackoverflow.com",
+		"http://golang.org",
+		"http://amazon.com",
+	}
+
+	for _, site := range sites {
+		checkSite(site)
+	}
+}
+
+func checkSite(site string) {
+	resp, err := http.Get(site)
+
+	if err != nil {
+		fmt.Println(site, " is down")
+		return
+	}
+
+	fmt.Println(resp.Status)
 }
