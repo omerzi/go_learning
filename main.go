2aae24dded11acd129a9f8a9892165e7ca54888b
Omer Zidkoni <omerzi@ad.checkpoint.com>
2022-06-08 12:58:31 +0300

    assignment 3

--- main.go
+++ main.go
@@ -1,5 +1,18 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"io"
+	"os"
+)
 
+func main() {
+	file, err := os.Open(os.Args[1])
+
+	if err != nil {
+		fmt.Println("An error occured: ", err)
+		os.Exit(1)
+	}
+
+	io.Copy(os.Stdout, file)
 }
